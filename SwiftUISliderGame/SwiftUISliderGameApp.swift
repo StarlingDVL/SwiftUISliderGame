ae72a36f8e730103bd818a4b17195a2330bde6c8
ISkvor <starlingy608@gmail.com>
2022-05-08 19:28:04 +0300

    Initial Commit

---
+++ SwiftUISliderGame/SwiftUISliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderGameApp.swift
+//  SwiftUISliderGame
+//
+//  Created by Юрий Скворцов on 08.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
